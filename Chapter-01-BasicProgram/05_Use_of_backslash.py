33fbd3b0fbaef495d55cba323c8e715f4b2f4219
iamnarendrasingh <nnnwithnnn@gmail.com>
2019-03-01 22:27:39 +0530

    What is variable

---
+++ Chapter-01-BasicProgram/05_Use_of_backslash.py
@@ -0,0 +1,12 @@
+#Use of Backslash
+print("What\'s your name?")
+
+#Use of \n
+print("This is first line.\nExample of new line This is second line.")
+
+#Use of \t
+print("This is first line.\tExample of tab This is second line.")
+
+#Use of \
+print("This is first line.\
+This is second line using slash")
---
+++ Chapter-01-BasicProgram/06_Use_Of_Variable.py
@@ -0,0 +1,5 @@
+#Use of variable in python
+i = 5
+print(i)
+i = i +2
+print(i)
