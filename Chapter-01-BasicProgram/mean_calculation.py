d4aa8c82907d3c24249317576fe4811e2fd6269d
Narendra Singh <narendraa.singhshekhawat@gmail.com>
2020-04-29 19:43:41 +0530

    how to calculate mean
    how to calculate mean

---
+++ Chapter-01-BasicProgram/mean_calculation.py
@@ -0,0 +1,7 @@
+num_list = [21, 11, 19, 3,11,5]
+# FInd sum of the numbers
+num_sum = sum(num_list)
+#divide the sum with length of the list
+mean = num_sum / len(num_list)
+print(num_list)
+print("Mean of the above list of numbers is: " + str(round(mean,2)))
